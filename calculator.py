34f5335e8f58a2671526fe7bb8710bd9cc7e318e
Yash <yashwant.polapragada@gmail.com>
2023-03-14 15:12:38 +0530

    Updated Calculator.py

--- calculator.py
+++ calculator.py
@@ -1,5 +1,10 @@
 import tkinter as tk
 import math
+import os
+
+if os.environ.get('DISPLAY','') == '':
+    print('no display found. Using :0.0')
+    os.environ.__setitem__('DISPLAY', ':0.0')
     
 root = tk.Tk()
 root.title('Calculator')
